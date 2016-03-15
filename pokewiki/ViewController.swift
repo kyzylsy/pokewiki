e200b435fbeeb6324019cd28d90c2b30ed0eea36
Liu Siyu <siyu.liu@yx-corp.com>
2016-03-15 12:01:47 +0800

    Initial Commit

---
+++ pokewiki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokewiki
+//
+//  Created by 刘斯宇 on 16/3/15.
+//  Copyright © 2016年 LSteven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
