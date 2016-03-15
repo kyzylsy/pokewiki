00837c373390faec7f6ccb17ec7e6858529c364b
Liu Siyu <siyu.liu@yx-corp.com>
2016-03-15 14:02:50 +0800

    stash

---
+++ pokewiki/PokemonDexTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonDexTableViewCell.swift
+//  pokewiki
+//
+//  Created by 刘斯宇 on 16/3/15.
+//  Copyright © 2016年 LSteven. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDexTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
